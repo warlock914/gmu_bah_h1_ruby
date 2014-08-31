26a8bfa3c71a95635c30279e15facf6b9e058ce1
Student <warlock914@gmail.com>
2014-08-30 22:33:53 -0400

    second commit

---
+++ Workspace/functions.rb
@@ -0,0 +1,36 @@
+# Return a reversed copy of the array
+def reverse(an_array)
+  # write your code here
+end
+
+# Return a map of letters and counts of letters
+# Letters should be lowercase before counting
+# For example, histogram('Hello') returns {'h'=>1,'e'=>1,'l'=>2,'o'=>1}
+def histogram(a_string)
+  # write your code here
+end
+
+# Sum all the numbers in the array
+# an_array can contain lots of different things
+# Be sure to only sum the numbers
+# (Hint: the is_a? method might be useful here)
+def sum_only_numbers(an_array)
+  # write your code here
+end
+
+# For i from 1 to 100, return an array.
+# The elements in the array should follow this algorithm:
+# If i is a multiple of 3, the element is 'Fizz'
+# If i is a multiple of 5, the element is 'Buzz'
+# If i is a multiple of 3 and 5, the element is 'FizzBuzz'
+# Otherwise, the element is simply the value of i
+# For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
+def fizzbuzz
+  # write your code here
+end
+
+# Uncomment each of these to test your functions
+# puts reverse([3,6,'dog']).inspect
+# puts histogram('The Quick brown fox').inspect
+# puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
+# puts fizzbuzz.join("\n")
